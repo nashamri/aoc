a40b99cfe3ad82ce0115525a7b75af6892793d3c
nashamri <designernasser@gmail.com>
2022-12-02 01:04:12 +0300

    clean devenv

--- devenv.nix
+++ devenv.nix
@@ -1,27 +1,8 @@
 { pkgs, ... }: {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
   packages = with pkgs; [
     git
-    cowsay
   ];
 
-  enterShell = ''
-    cowsay "Hello AOCer"
-  '';
-
-  # https://devenv.sh/languages/
   languages.nix.enable = true;
   languages.python.enable = true;
-
-  # https://devenv.sh/scripts/
-  #scripts.hello.exec = "echo hello from $GREET";
-
-  # https://devenv.sh/pre-commit-hooks/
-  #pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  #processes.ping.exec = "ping example.com";
 }
