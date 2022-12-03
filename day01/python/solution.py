4d0c5ae2e0c2675b784a0683e751f95199407fa5
nashamri <designernasser@gmail.com>
2022-12-03 23:18:48 +0300

    day01: better solution? I guess not, but it's more compact

--- day01/python/solution.py
+++ day01/python/solution.py
@@ -1,13 +1,10 @@
-temp = []
-cals = []
-
+cals = [[]]
 with open("input.txt") as f:
-    for line in f.readlines():
-        if line != "\n":
-            temp.append(int(line))
-        else:
-            cals.append(sum(temp))
-            temp = []
+    [
+        cals[-1].append(int(x.strip())) if x != "\n" else cals.append([])
+        for x in f.readlines()
+    ]
+    cals = sorted(map(sum, cals))
 
-print(f"Part 1: {sorted(cals)[-1]}")
-print(f"Part 2: {sum(sorted(cals)[-3:])}")
+print(f"Part 1: {sorted(cals)[-1]}")  # 70116
+print(f"Part 2: {sum(sorted(cals)[-3:])}")  # 206582
