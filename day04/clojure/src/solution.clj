8a50c4d473851640512917e4f234c4eb59938b24
nashamri <designernasser@gmail.com>
2022-12-05 22:34:53 +0300

    day04: part 1 solved in clojure

---
+++ day04/clojure/src/solution.clj
@@ -0,0 +1,48 @@
+(ns solution
+  (:require [clojure.string :as str]
+            [clojure.set :as s]))
+
+(def sample
+  "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8")
+
+(defn parse [data]
+  (->> data
+       (#(str/split % #"\n"))
+       (mapv #(str/split % #","))
+       (mapv (fn [x] (mapv #(str/split % #"-") x)))
+       (mapv (fn [x] (mapv (fn [y] (mapv parse-long y)) x)))
+       (mapv (fn [[[x1 y1] [x2 y2]]] [[x1 (inc y1)] [x2 (inc y2)]]))))
+
+(defn common []
+  (->> (slurp "input.txt") (parse)))
+
+(defn part1-sol []
+  (get (frequencies
+        (mapv
+         (fn [[x y]]
+           (or (s/subset?
+                (set (apply range x))
+                (set (apply range y)))
+               (s/subset?
+                (set (apply range y))
+                (set (apply range x)))))
+
+         (common)))
+       true))
+
+(defn part2-sol [])
+
+(comment
+  ;
+  )
+(defn -main
+  "Invoke me with clojure -M -m solution"
+  [& _]
+  (println (str "Part 1: " (part1-sol))) ;
+  (println (str "Part 2: " (part2-sol))) ;
+  )
