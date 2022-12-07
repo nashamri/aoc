3326d8e3ed4e7746353f6dad2baec01178a2e55e
nashamri <designernasser@gmail.com>
2022-12-07 16:46:41 +0300

    day06: solved in clojure

---
+++ day06/clojure/src/solution.clj
@@ -0,0 +1,32 @@
+(ns solution
+  (:require [clojure.string :as str]))
+
+(def sample "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+
+(defn common []
+  (->> (slurp "input.txt")))
+
+(defn part1-sol []
+  (->> (common)
+       (partition 4 1)
+       (map-indexed (fn [idx itm] [idx itm (apply distinct? itm)]))
+       (filter (fn [[_ _ pred]] pred))
+       (first)
+       ((fn [[i _ _]] (+ i 4)))))
+
+(defn part2-sol []
+  (->> (common)
+       (partition 14 1)
+       (map-indexed (fn [idx itm] [idx itm (apply distinct? itm)]))
+       (filter (fn [[_ _ pred]] pred))
+       (first)
+       ((fn [[i _ _]] (+ i 14)))))
+
+(comment)
+
+(defn -main
+  "Invoke me with clojure -M -m solution"
+  [& _]
+  (println (str "Part 1: " (part1-sol))) ; "Part 1: 1707"
+  (println (str "Part 2: " (part2-sol))) ; "Part 2: 3697"
+  )
