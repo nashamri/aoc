a938f2105792ef3e5a2f4a5ea0ac88b7a98ff117
nashamri <designernasser@gmail.com>
2023-11-18 00:23:49 +0300

    add uiua

--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,19 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  uiuapkg,
+  ...
+}: {
   packages = with pkgs; [
     git
     python310Packages.black
     nodePackages.pyright
+    uiuapkg.packages.x86_64-linux.default
   ];
 
   languages = {
     nix.enable = true;
     python.enable = true;
-    haskell.enable = true;
+    # haskell.enable = true;
     clojure.enable = true;
   };
 
