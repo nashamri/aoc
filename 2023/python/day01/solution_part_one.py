b564e0624163899b4fa94a7872e126f99f168d34
@Githuba93bpasswordbaltare5 <farisala@infinitepl.com>
2023-12-01 19:01:09 +0300

    re-named for better readabilty

---
+++ 2023/python/day01/solution_part_one.py
@@ -0,0 +1,13 @@
+numbers = []
+
+def keep_numbers_only(input_string):
+    return ''.join(filter(str.isdigit, input_string))
+
+with open("sample.txt","r") as file:
+    for line in file:
+        number = str(keep_numbers_only(line))
+        first_digit = number[0]
+        last_digit = number[-1]
+        numbers.append(int(first_digit+last_digit))
+
+print(sum(numbers))
